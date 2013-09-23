a46846eb9b5d1a7916e1576a159e879a7aca34b8
Michael Sumner <michael.sumner@aad.gov.au>
2013-09-24 08:44:22 +1000

    read palettes from SEADAS

--- R/datapal.R
+++ R/datapal.R
@@ -92,3 +92,17 @@ seaice.de <- function(x, breaks = FALSE) {
 ##' @format A list with vectors \code{col} and \code{breaks}.
 ##' @keywords data
 NULL
+
+read.cpd <-
+function(x) {
+        txt <- readLines(x)
+        cold <- read.table(text = sapply(strsplit(grep("^color", txt, value = TRUE), "="), "[", 2), sep = ",")
+        cold$value <- as.numeric(sapply(strsplit(grep("^sample", txt, value = TRUE), "="), "[", 2))
+
+        list(breaks = cold$value, col = rgb(cold[,1], cold[,2], cold[, 3], max = 255))
+}
+
+##up <- file.path("C:/Users/mdsumner", ".seadas", "beam-ui", "auxdata", "color-palettes")
+##fs <- list.files(up, pattern = "cpd$")
+##read.cpd(file.path(up, fs[1]))
+
