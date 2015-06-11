4cf96cdbcc74e11d251dd7d9fb818c600622ba44
Michael Sumner <mdsumner@gmail.com.com>
2015-06-12 00:20:58 +1000

    move oisst to extdata

--- inst/extdata/create_oisst.R
+++ inst/extdata/create_oisst.R
@@ -5,4 +5,4 @@
 #   oisst <- readsst(f$date, xylim = extent(140, 180, -65, -30))
 #   metadata(oisst) <- list(ncdump = local({suppressWarnings(r <- raster(f$fullname)); capture.output(print(r))[-1]}), 
 #  filename = gsub("data/", "", f$file))
-#   save(oisst, file = "data/oisst.rda", compress = "bzip2")
+#   save(oisst, file = "inst/extdata/oisst.rda", compress = "bzip2")
